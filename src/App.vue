8e23e20083220f96fc73893985144698f28fb187
filippo199600 <pippotartaglia@gmail.com>
2023-12-31 11:27:49 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+import Welcome from "./components/Welcome.vue";
+import Projects from "./components/Projects.vue";
+import AboutMe from "./components/AboutMe.vue";
+import Footer from "./components/ui/Footer.vue";
+
+export default {
+  data() {
+    return {
+      title: "Il gestionale",
+    };
+  },
+
+  components: {
+    Welcome,
+    Projects,
+    AboutMe,
+    Footer,
+  },
+};
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+  <Welcome></Welcome>
+  <Projects></Projects>
+  <AboutMe></AboutMe>
+  <Footer></Footer>
+</template>
+
+<style lang="scss"></style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from "vue";
+import "./assets/scss/style.scss";
+import * as Bootstrap from "bootstrap";
+
+import App from "./App.vue";
+
+createApp(App).mount("#app");

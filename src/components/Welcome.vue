5600e7a1f153e7b840cd0670d6dfdbd3f1da9ecb
filippo199600 <pippotartaglia@gmail.com>
2024-01-02 19:26:11 +0100

    add welcome animation

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -5,11 +5,23 @@ export default {
       title: "Welcome",
     };
   },
+
+  mounted() {
+    const gradientElement = this.$el.querySelector(".absolute-gradient");
+
+    const desiredHeight = window.innerHeight * 0.63;
+
+    // Applica l'altezza desiderata all'elemento .absolute-gradient dopo un piccolo ritardo per consentire la transizione
+    setTimeout(() => {
+      gradientElement.style.height = `${desiredHeight}px`;
+    }, 100); // Puoi modificare il ritardo se necessario per ottenere l'effetto desiderato
+  },
 };
 </script>
 
 <template>
   <div class="wrapper">
+    <div class="absolute-gradient"></div>
     <div class="container">
       <div class="header-text">
         <div class="main-content">
@@ -34,19 +46,60 @@ export default {
 @use "../assets/scss/variables.scss" as *;
 .wrapper {
   height: 110vh;
-  background: linear-gradient(0deg, $shade-100 52%, $background-color 50%);
+  background: $background-color;
+  overflow: hidden;
+  position: relative;
+
+  .absolute-gradient {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    width: 100%;
+    height: 45%;
+    z-index: 0;
+    background: $shade-100;
+    transition: height 3s linear;
+  }
   .container {
     height: 100%;
     .header-text {
+      position: absolute;
       height: 100vh;
       display: flex;
       flex-direction: column;
       justify-content: center;
+      z-index: 1;
       .main-content {
         height: 100%;
         display: flex;
         flex-direction: column;
         justify-content: center;
+
+        .author-name {
+          font-size: 1.5rem;
+          color: $grey-text;
+        }
+        .title {
+          color: $title-welcome;
+        }
+        .subtitle {
+          color: $shade-500;
+          animation: changeColor 4s linear;
+        }
+        @keyframes changeColor {
+          0% {
+            color: $grey-text; /* Colore di partenza */
+          }
+          100% {
+            color: $shade-500; /* Colore finale */
+          }
+        }
+      }
+      .scroll-button {
+        a {
+          text-decoration: none;
+        }
       }
     }
   }

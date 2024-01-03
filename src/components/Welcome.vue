96b4439c801316727916823996cf07d9d50d6f27
filippo199600 <pippotartaglia@gmail.com>
2024-01-03 10:30:24 +0100

    add animation

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -6,10 +6,14 @@ export default {
     };
   },
 
+  created() {
+    window.scrollTo(0, 0);
+  },
+
   mounted() {
     const gradientElement = this.$el.querySelector(".absolute-gradient");
 
-    const desiredHeight = window.innerHeight * 0.63;
+    const desiredHeight = window.innerHeight * 0.64;
 
     // Applica l'altezza desiderata all'elemento .absolute-gradient dopo un piccolo ritardo per consentire la transizione
     setTimeout(() => {
@@ -20,7 +24,7 @@ export default {
 </script>
 
 <template>
-  <div class="wrapper">
+  <div class="wrapper" id="welcome">
     <div class="absolute-gradient"></div>
     <div class="container">
       <div class="header-text">
@@ -56,13 +60,13 @@ export default {
     left: 0;
     right: 0;
     width: 100%;
-    height: 45%;
+    height: 0%;
     z-index: 0;
     background: $shade-100;
-    transition: height 3s linear;
+    transition: height 2s linear;
   }
   .container {
-    height: 100%;
+    height: 100vh;
     .header-text {
       position: absolute;
       height: 100vh;
@@ -79,9 +83,34 @@ export default {
         .author-name {
           font-size: 1.5rem;
           color: $grey-text;
+          transform-origin: left;
+          transform: perspective(400px) rotateY(90deg);
+          opacity: 0;
+          animation: openBook 2s ease forwards;
+        }
+        @keyframes openBook {
+          0% {
+            opacity: 0;
+            transform: perspective(400px) rotateY(90deg);
+          }
+          100% {
+            opacity: 1;
+            transform: perspective(400px) rotateY(0deg);
+          }
         }
         .title {
           color: $title-welcome;
+          position: relative;
+          left: 100%;
+          animation: slideLeft 1.5s ease forwards;
+        }
+        @keyframes slideLeft {
+          0% {
+            left: 100%;
+          }
+          100% {
+            left: 0;
+          }
         }
         .subtitle {
           color: $shade-500;
@@ -99,6 +128,30 @@ export default {
       .scroll-button {
         a {
           text-decoration: none;
+          position: relative;
+          display: inline-block;
+
+          font-size: 1.5rem;
+          transition: transform 0.3s ease-out;
+
+          &:hover {
+            animation: bounce 1.5s ease infinite;
+          }
+        }
+      }
+      @keyframes bounce {
+        0%,
+        20%,
+        50%,
+        80%,
+        100% {
+          transform: translateY(0);
+        }
+        40% {
+          transform: translateY(-12px);
+        }
+        60% {
+          transform: translateY(-8px);
         }
       }
     }
